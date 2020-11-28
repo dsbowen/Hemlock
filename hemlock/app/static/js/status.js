3a25ca0bc3e2c257bd7dd65486b53a8c0542c504
dsbowen <dsbowen@wharton.upenn.edu>
2020-11-28 16:06:30 -0500

    deploying algorithm

--- hemlock/app/static/js/status.js
+++ hemlock/app/static/js/status.js
@@ -1,17 +1,30 @@
 // Statis page js
 // This function listens for status updates and updates the researcher page
 
-$(document).ready(function(){
-    var socket = io.connect(window.location.origin+"/participants-nsp");
-    socket.on("connect", function(){
-        console.log("Socket connected");
-    });
-    socket.on("json", function(e){
-        console.log("Received status update "+e);
-        var curr_status = JSON.parse(e);
-        $("#Completed").text(curr_status.Completed);
-        $("#InProgress").text(curr_status.InProgress);
-        $("#TimedOut").text(curr_status.TimedOut);
-        $("#Total").text(curr_status.Total);
-    });
-}); 
+// $(document).ready(function(){
+//     var socket = io.connect(window.location.origin+"/participants-nsp");
+//     socket.on("connect", function(){
+//         console.log("Socket connected");
+//     });
+//     socket.on("json", function(e){
+//         console.log("Received status update "+e);
+//         var curr_status = JSON.parse(e);
+//         $("#Completed").text(curr_status.Completed);
+//         $("#InProgress").text(curr_status.InProgress);
+//         $("#TimedOut").text(curr_status.TimedOut);
+//         $("#Total").text(curr_status.Total);
+//     });
+// }); 
+
+var socket = io.connect(window.location.origin+"/participants-nsp");
+socket.on("connect", function(){
+    console.log("Socket connected");
+});
+socket.on("json", function(e){
+    console.log("Received status update "+e);
+    var curr_status = JSON.parse(e);
+    $("#Completed").text(curr_status.Completed);
+    $("#InProgress").text(curr_status.InProgress);
+    $("#TimedOut").text(curr_status.TimedOut);
+    $("#Total").text(curr_status.Total);
+});
