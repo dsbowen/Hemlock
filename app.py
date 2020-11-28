55423b42fb4f44590f0a2ddcd4dd3bd9f20ca97e
dsbowen <dsbowen@wharton.upenn.edu>
2020-11-28 12:53:20 -0500

    deploying algorithm

--- app.py
+++ app.py
@@ -12,3 +12,10 @@ app = create_app()
 if __name__ == '__main__':
     from hemlock.app import socketio
     socketio.run(app, debug=True)
+
+# ,
+#     "buildpacks": [
+#         {"url": "heroku/python"},
+#         {"url": "https://github.com/heroku/heroku-buildpack-chromedriver"},
+#         {"url": "https://github.com/heroku/heroku-buildpack-google-chrome"}
+#     ]
